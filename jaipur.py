54527d8f2ded3767850d3d3be3cb71a43624c273
Daniel Ekeroth <edaneke@utb.hoglandet.se>
2018-04-08 15:48:45 +0200

    Init commit TETRIS

--- jaipur.py
+++ jaipur.py
@@ -1,7 +1,7 @@
 import pygame
 import logging
 import sys
-from random import shuffle
+from random import shuffle, randint
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -12,7 +12,8 @@ PLAYFIELDCOLOR = [pygame.Color('#000000'), pygame.Color('#75646A')]
 
 SHAPESCOLOR = {1: pygame.Color('#A36D7F'),
                2: pygame.Color('#A37F6D'),
-               3: pygame.Color('#7FA36D'),}
+               3: pygame.Color('#7FA36D'),
+               10: pygame.Color('#FFFFFF')}
 
 class Controller():
 
@@ -59,12 +60,14 @@ class Controller():
             # Update state ----------------------------------------
             if self.game_state == Controller.INIT:
 #                //PRELOADEBLE STUFF
-                self.playfield = Playfield(self.screen)
+                self.playfield = Playfield(self)
+                self.geoforms = Geoforms(self)
                 self.game_state = Controller.RUNNING
                 # self.matrix = self.playfield.reset()
 
             if self.game_state == Controller.RUNNING:
                 self.playfield.tick()
+                self.geoforms.tick()
 
 
             # Draw everything on screen ------------------------
@@ -73,11 +76,12 @@ class Controller():
 
             if self.game_state == Controller.RUNNING:
                 self.playfield.draw()
-#                self.geoforms.draw()
+                self.playfield.stopped_shapes()
+                self.geoforms.draw()
 #                self.geoforms.update()
 
             pygame.display.flip()
-            self.clock.tick(5)
+            self.clock.tick(10)
 
 
     def quit(self, event):
@@ -102,77 +106,113 @@ class Controller():
 
 class Geoforms():
 
-    def __init__(self, screen):
-        self.screen = screen
-        self.yspeed = -1
-        self.x = self.screen.get_width() / 2
+    def __init__(self, controller):
+        self.controller = controller
+        self.screen = controller.screen
+        self.x = 4
+        self.y = 10
 
-    def hit(self, screen):
-        # Check if geoforms is hitting the floor or the edge
-        pass
+        self.playfield_position = (SCREEN_SIZE[0] / 2  - 300 / 2,
+                                   SCREEN_SIZE[1] / 2 - 600 / 2)
+        self.num_ticks = 0
+        self.geoform = Geoforms._generate_shape()
 
-
-    def update(self):
-        pass
+    @staticmethod
+    def _generate_shape():
+        forms = {'T': [[0, 1], [-1, 0], [0, 0], [1, 0]],
+                      'I': [[1, 0], [0, 0], [2, 0], [-1, 0]],
+                      'H': [[1, 0], [0, 1], [1, 1], [0, 0]],
+                      'Z': [[0, 1], [0, 0], [1, 0], [1, -1]],
+                      'L': [[0, 0], [1, 0], [0, 1], [0, 2]]}
+        dic = {1: 'T',
+               2: 'I',
+               3: 'H',
+               4: 'Z',
+               5: 'L'}
+        num = randint(1,5)
+        return forms[dic[num]]
 
     def draw(self):
-        surface = pygame.Surface((self.x, self.screen.get_height()))
-        pygame.draw.rect(surface, SHAPESCOLOR[1], (0, 0, self.x, 10))
-        self.screen.blit(surface, (self.x, 0))
-    #Make KEYARROW events
+        surface = pygame.Surface((300, 600), pygame.SRCALPHA)
+
+        for xo, yo in self.geoform:
+            pygame.draw.rect(surface,
+                             SHAPESCOLOR[2],
+                             (30 * (self.x + xo) + 2, 30 * (19 - (self.y + yo)) + 2, 26, 26))
+
+
+        self.screen.blit(surface, self.playfield_position)
+
+    def tick(self):
+        self.num_ticks = self.num_ticks + 1 if self.num_ticks < 10 else 0
+
+        if self.num_ticks == 0:
+            if self.valid_move():
+                self.y -= 1
+            else:
+                self.controller.playfield.shape = self.geoform
+                logger.debug(self.controller.playfield.shape)
+                self.controller.playfield.shapes_S[(self.x, self.y)] = self.geoform
+                self.geoform = Geoforms._generate_shape()
+                #logger.debug('X Value: ' '{}', 'Y Value: ' '{}'.format(self.x, self.y))
+                self.y = 19
+                self.x = 4
+
+
+    def valid_move(self):
+        for xo, yo in self.geoform:
+            if self.controller.playfield.matrix[self.y + yo - 1][self.x + xo - 1] != 0:
+                return False
+        return True
 
     def mouseup(self, event):
         logger.debug('Mouse up on card {}'.format(self))
 
 class Playfield():
-    def __init__(self, screen):
-        self.screen = screen
-        self.winx = SCREEN_SIZE[0] / 2 - 150
-        self.winy = SCREEN_SIZE[1] / 2 - 300
-
-        self.reset()
-
-        self.rectangle()
-    def longShape(self):
-
-        self.matrix[19][3] = 2
-        self.matrix[19][4] = 2
-        self.matrix[19][5] = 2
-        self.matrix[19][6] = 2
-
-    def rectangle(self):
-        self.matrix[18][3] = 1
-        self.matrix[18][4] = 1
-        self.matrix[18][5] = 1
-        self.matrix[19][4] = 1
-
-    def reset(self):
+    def __init__(self, controller):
+        self.playfield_position = (SCREEN_SIZE[0] / 2  - 300 / 2,
+                                   SCREEN_SIZE[1] / 2 - 600 / 2)
+        self.controller = controller
+        self.shapex = 0
+        self.shapey = 0
+        self.screen = self.controller.screen
         self.matrix = [[0 for x in range(10)] for y in range(20)]
+        self.shape = 0
+        self.shapes_S = {}
+        self.testx = 0
+        self.testy = 0
+        #Test shape ---->
+        self.matrix[0][3] = 3
+        self.matrix[0][4] = 3
+        self.matrix[0][5] = 3
+        self.matrix[1][4] = 3
 
-    def tick(self):
-        for row in range(20):
-            for column in range(10):
-                if self.matrix[row][column] > 0:
-                    if row == 0 or self.matrix[row - 1][column] == 1:
-                        self.longShape()
-                        break
-                    else:
-                        self.matrix[row - 1][column] = 1
-                        self.matrix[row][column] = 0
+        logger.debug(self.matrix)
     def draw(self):
         surface = pygame.Surface((300, 600))
         surface.fill(PLAYFIELDCOLOR[1])
 
+        self.screen.blit(surface, (self.playfield_position))
+
+
+    def tick(self):
+        if len(self.shapes_S) > 0:
+            for key, value in self.shapes_S.items():
+                self.testy = key[1]
+                self.testx = key[0]
+                for xo, yo in value:
+                    self.matrix[key[0] + xo][key[1] + yo] = randint(1,3)
+
+    def stopped_shapes(self):
+        surface = pygame.Surface((300, 600), pygame.SRCALPHA)
         for row in range(20):
             for column in range(10):
-                logger.debug('({}, {}): {}'.format(row, column, self.matrix[row][column]))
                 if self.matrix[row][column] > 0:
-                    rect = (column * 30, -1 * (row - 19) * 30, 29, 29)
-                    logger.debug(rect)
-                    pygame.draw.rect(surface, SHAPESCOLOR[self.matrix[row][column]], rect)
-
-        self.screen.blit(surface, (self.winx, self.winy))
-
+                    pygame.draw.rect(surface,
+                    SHAPESCOLOR[1],
+                    (30 * (column) + 2, 30 * (19 - (row)) + 2, 26, 26))
+                    self.shapes_S = {}
+                    self.screen.blit(surface, (self.playfield_position))
 
 if __name__ == "__main__":
     c = Controller()
